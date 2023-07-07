9769715295e3f86a4860be6a023c7ed6061a1c77
耗子 <i@haozi.net>
2023-07-07 18:08:34 +0800

    fix: LICENSE

--- main.go
+++ main.go
@@ -1,5 +1,5 @@
 /*
-Copyright [2022] [HaoZi Technology Co., Ltd.]
+Copyright 2022 HaoZi Technology Co., Ltd.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
--- scripts/plugins/openresty/install.sh
+++ scripts/plugins/openresty/install.sh
@@ -2,7 +2,7 @@
 export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:$PATH
 
 : '
-Copyright [2022] [HaoZi Technology Co., Ltd.]
+Copyright 2022 HaoZi Technology Co., Ltd.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
