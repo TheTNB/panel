1f5718cd67043000acc89ac1dad70c7d5e5c4c57
耗子 <i@haozi.net>
2023-07-20 01:46:05 +0800

    fix(cmd): conf path

--- scripts/panel.sh
+++ scripts/panel.sh
@@ -16,4 +16,4 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 '
-/www/panel/panel --env="panel.conf" artisan panel "$@"
+/www/panel/panel --env="/www/panel/panel.conf" artisan panel "$@"
