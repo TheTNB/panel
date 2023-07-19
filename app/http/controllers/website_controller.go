bce2f27500b829198d8ff129f160e6185fb9fd1d
耗子 <i@haozi.net>
2023-07-20 01:10:27 +0800

    feat(website controller): optimize ssl config

--- app/http/controllers/website_controller.go
+++ app/http/controllers/website_controller.go
@@ -315,7 +315,6 @@ func (c *WebsiteController) SaveConfig(ctx http.Context) {
     ssl_session_timeout 1d;
     ssl_session_cache shared:SSL:10m;
     ssl_session_tickets off;
-    ssl_dhparam /etc/ssl/certs/dhparam.pem;
     ssl_protocols TLSv1.2 TLSv1.3;
     ssl_ciphers ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384;
     ssl_prefer_server_ciphers off;
