2fa108dd67b33379b22dbbfebdbe5cc4bfdbe572
耗子 <i@haozi.net>
2023-08-13 22:38:39 +0800

    feat: remove unnecessary bootstrap

--- bootstrap/app.go
+++ bootstrap/app.go
@@ -2,7 +2,6 @@ package bootstrap
 
 import (
 	"github.com/goravel/framework/foundation"
-	"github.com/goravel/framework/support/carbon"
 
 	"panel/config"
 )
@@ -15,7 +14,4 @@ func Boot() {
 
 	// Bootstrap the config.
 	config.Boot()
-
-	// 设置 Carbon 时区
-	carbon.SetTimezone(carbon.PRC)
 }
