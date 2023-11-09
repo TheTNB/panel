63c4e27806f968fec2ef315fcf9c0109282248c3
耗子 <i@haozi.net>
2023-11-10 01:49:05 +0800

    fix: test

---
+++ tests/setting/setting_test.go
@@ -0,0 +1,38 @@
+package setting
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+
+	"panel/app/services"
+	"panel/tests"
+)
+
+type SettingTestSuite struct {
+	suite.Suite
+	tests.TestCase
+	setting services.Setting
+}
+
+func TestSettingTestSuite(t *testing.T) {
+	suite.Run(t, &SettingTestSuite{
+		setting: services.NewSettingImpl(),
+	})
+}
+
+func (s *SettingTestSuite) SetupTest() {
+
+}
+
+func (s *SettingTestSuite) TestGet() {
+	a := s.setting.Get("test")
+	b := s.setting.Get("test", "test")
+	s.Equal("", a)
+	s.Equal("test", b)
+}
+
+func (s *SettingTestSuite) TestSet() {
+	err := s.setting.Set("test", "test")
+	s.Nil(err)
+}
--- tests/user/user_test.go
+++ tests/user/user_test.go
@@ -1,19 +1,23 @@
-package services
+package user
 
 import (
 	"testing"
 
 	"github.com/stretchr/testify/suite"
+
+	"panel/app/services"
+	"panel/tests"
 )
 
 type UserTestSuite struct {
 	suite.Suite
-	user User
+	tests.TestCase
+	user services.User
 }
 
 func TestUserTestSuite(t *testing.T) {
 	suite.Run(t, &UserTestSuite{
-		user: NewUserImpl(),
+		user: services.NewUserImpl(),
 	})
 }
 
@@ -22,13 +26,7 @@ func (s *UserTestSuite) SetupTest() {
 }
 
 func (s *UserTestSuite) TestCreate() {
-	/*mockOrm, mockDb, _, _ := mock.Orm()
-	mockOrm.On("Query").Return(mockDb).Once()
-	mockDb.On("Create", &models.User{
-		Username: "haozi",
-		Password: "123456",
-	}).Return(nil).Once()
 	user, err := s.user.Create("haozi", "123456")
 	s.Nil(err)
-	s.Equal("haozi", user.Username)*/
+	s.Equal("haozi", user.Username)
 }
