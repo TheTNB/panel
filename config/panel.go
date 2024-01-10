bb23be565a430f67e92fb9b7c6e7f3d6f75ee9ed
耗子 <i@haozi.net>
2024-01-10 13:57:00 +0800

    feat: 发布 v2.1.36

--- config/panel.go
+++ config/panel.go
@@ -8,7 +8,7 @@ func init() {
 	config := facades.Config()
 	config.Add("panel", map[string]any{
 		"name":    "耗子 Linux 面板",
-		"version": "v2.1.34",
+		"version": "v2.1.36",
 		"ssl":     config.Env("APP_SSL", false),
 	})
 }
