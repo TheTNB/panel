27cbb92a06e8161b4dc8c3058fa0d8472bd66ae9
耗子 <i@haozi.net>
2024-06-14 21:00:12 +0800

    fix: build

--- app/providers/database_service_provider.go
+++ app/providers/database_service_provider.go
@@ -1,9 +1,13 @@
 package providers
 
 import (
+	"fmt"
+
 	"github.com/goravel/framework/contracts/database/seeder"
 	"github.com/goravel/framework/contracts/foundation"
 	"github.com/goravel/framework/facades"
+
+	"github.com/TheTNB/panel/app/models"
 )
 
 type DatabaseServiceProvider struct {
@@ -15,4 +19,19 @@ func (receiver *DatabaseServiceProvider) Register(app foundation.Application) {
 
 func (receiver *DatabaseServiceProvider) Boot(app foundation.Application) {
 	facades.Seeder().Register([]seeder.Seeder{})
+	if err := facades.Orm().Query().AutoMigrate(
+		&models.Cert{},
+		&models.CertDNS{},
+		&models.CertUser{},
+		&models.Cron{},
+		&models.Database{},
+		&models.Monitor{},
+		&models.Plugin{},
+		&models.Setting{},
+		&models.Task{},
+		&models.User{},
+		&models.Website{},
+	); err != nil {
+		panic(fmt.Sprintf("Failed to migrate database: %v", err))
+	}
 }
