5ddbdfc5df80b3847e13f7bad330e1b7be7f1bcd
耗子 <i@haozi.net>
2024-06-14 19:58:49 +0800

    fix: lint

--- app/providers/database_service_provider.go
+++ app/providers/database_service_provider.go
@@ -4,8 +4,6 @@ import (
 	"github.com/goravel/framework/contracts/database/seeder"
 	"github.com/goravel/framework/contracts/foundation"
 	"github.com/goravel/framework/facades"
-
-	"github.com/TheTNB/panel/database/seeders"
 )
 
 type DatabaseServiceProvider struct {
@@ -16,7 +14,5 @@ func (receiver *DatabaseServiceProvider) Register(app foundation.Application) {
 }
 
 func (receiver *DatabaseServiceProvider) Boot(app foundation.Application) {
-	facades.Seeder().Register([]seeder.Seeder{
-		&seeders.DatabaseSeeder{},
-	})
+	facades.Seeder().Register([]seeder.Seeder{})
 }
