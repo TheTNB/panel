aa9b55d1eb9f48a5130e14ac4d67a13c2ea43c18
耗子 <i@haozi.net>
2024-06-19 02:06:58 +0800

    feat: bump v2.2.11

--- config/panel.go
+++ config/panel.go
@@ -8,7 +8,7 @@ func init() {
 	config := facades.Config()
 	config.Add("panel", map[string]any{
 		"name":    "耗子面板",
-		"version": "v2.2.10",
+		"version": "v2.2.11",
 		"ssl":     config.Env("APP_SSL", false),
 	})
 }
