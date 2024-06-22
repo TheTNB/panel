f2e8ff86c6cedf8f29ad120304d87714dff2cf7d
耗子 <i@haozi.net>
2024-06-23 03:19:39 +0800

    feat: 发布v2.2.13

--- config/panel.go
+++ config/panel.go
@@ -8,7 +8,7 @@ func init() {
 	config := facades.Config()
 	config.Add("panel", map[string]any{
 		"name":    "耗子面板",
-		"version": "v2.2.12",
+		"version": "v2.2.13",
 		"ssl":     config.Env("APP_SSL", false),
 	})
 }
