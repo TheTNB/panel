7e18d75203033341665a3f4188eb6c47b82e7490
耗子 <i@haozi.net>
2024-10-10 00:24:36 +0800

    chore: remove eslint-plugin-import

--- web/.eslintrc.cjs
+++ web/.eslintrc.cjs
@@ -3,16 +3,6 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  plugins: ['import'],
-  rules: {
-    'import/order': [
-      'error',
-      {
-        groups: [['builtin', 'external', 'internal']],
-        'newlines-between': 'always'
-      }
-    ]
-  },
   extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
