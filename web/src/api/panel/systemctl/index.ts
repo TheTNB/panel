cfc1d46381e944b0d5372305ce12fc032a0d2b76
耗子 <i@haozi.net>
2024-10-11 02:03:10 +0800

    fix: git

--- web/src/api/panel/systemctl/index.ts
+++ web/src/api/panel/systemctl/index.ts
@@ -5,26 +5,26 @@ import { request } from '@/utils'
 export default {
   // 服务状态
   status: (service: string): Promise<AxiosResponse<any>> =>
-    request.get('/system/service/status', { params: { service } }),
+    request.get('/systemctl/status', { params: { service } }),
   // 是否启用服务
   isEnabled: (service: string): Promise<AxiosResponse<any>> =>
-    request.get('/system/service/isEnabled', { params: { service } }),
+    request.get('/systemctl/isEnabled', { params: { service } }),
   // 启用服务
   enable: (service: string): Promise<AxiosResponse<any>> =>
-    request.post('/system/service/enable', { service }),
+    request.post('/systemctl/enable', { service }),
   // 禁用服务
   disable: (service: string): Promise<AxiosResponse<any>> =>
-    request.post('/system/service/disable', { service }),
+    request.post('/systemctl/disable', { service }),
   // 重启服务
   restart: (service: string): Promise<AxiosResponse<any>> =>
-    request.post('/system/service/restart', { service }),
+    request.post('/systemctl/restart', { service }),
   // 重载服务
   reload: (service: string): Promise<AxiosResponse<any>> =>
-    request.post('/system/service/reload', { service }),
+    request.post('/systemctl/reload', { service }),
   // 启动服务
   start: (service: string): Promise<AxiosResponse<any>> =>
-    request.post('/system/service/start', { service }),
+    request.post('/systemctl/start', { service }),
   // 停止服务
   stop: (service: string): Promise<AxiosResponse<any>> =>
-    request.post('/system/service/stop', { service })
+    request.post('/systemctl/stop', { service })
 }
