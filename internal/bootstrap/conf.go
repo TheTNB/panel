78543dac677644b38bb0f8807f1e61f4b10b7334
耗子 <i@haozi.net>
2024-10-18 02:11:58 +0800

    feat: 发布v2.3.9

--- internal/bootstrap/conf.go
+++ internal/bootstrap/conf.go
@@ -26,7 +26,7 @@ func initConf() {
 
 func initGlobal() {
 	app.Root = app.Conf.MustString("app.root")
-	app.Version = "2.3.8"
+	app.Version = "2.3.9"
 	app.Locale = app.Conf.MustString("app.locale")
 
 	// 初始化时区
