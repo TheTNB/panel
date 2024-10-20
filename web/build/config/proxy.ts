892f91be3ebef618667c35e97ba39fb937574bad
耗子 <i@haozi.net>
2024-10-20 19:55:14 +0800

    feat: 开发服务器代理支持多目标

--- web/build/config/proxy.ts
+++ web/build/config/proxy.ts
@@ -1,17 +1,20 @@
 import type { ProxyOptions } from 'vite'
-import { getProxyConfig } from '../../settings/proxy-config'
+import { getProxyConfigs } from '../../settings/proxy-config'
 
 export function createViteProxy(isUseProxy = true, proxyType: ProxyType) {
   if (!isUseProxy) return undefined
 
-  const proxyConfig = getProxyConfig(proxyType)
-  const proxy: Record<string, string | ProxyOptions> = {
-    [proxyConfig.prefix]: {
+  const proxyConfigs = getProxyConfigs(proxyType)
+  const proxy: Record<string, string | ProxyOptions> = {}
+
+  proxyConfigs.forEach((proxyConfig) => {
+    proxy[proxyConfig.prefix] = {
       target: proxyConfig.target,
       secure: proxyConfig.secure,
       changeOrigin: true,
       rewrite: (path: string) => path.replace(new RegExp(`^${proxyConfig.prefix}`), '')
     }
-  }
+  })
+
   return proxy
 }
