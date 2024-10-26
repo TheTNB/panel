2a61f8939fb34dadf2d156b7b92a1969db8705d2
耗子 <i@haozi.net>
2024-10-27 04:37:38 +0800

    fix: PHP关闭状态下获取负载报错

--- internal/apps/php/service.go
+++ internal/apps/php/service.go
@@ -87,7 +87,7 @@ func (s *Service) Load(w http.ResponseWriter, r *http.Request) {
 	client := resty.New().SetTimeout(10 * time.Second)
 	resp, err := client.R().Get(fmt.Sprintf("http://127.0.0.1/phpfpm_status/%d", s.version))
 	if err != nil || !resp.IsSuccess() {
-		service.Error(w, http.StatusInternalServerError, "获取负载状态失败：%v", err)
+		service.Success(w, []types.NV{})
 		return
 	}
 
