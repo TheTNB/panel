a28fc592d183ad3e03260881f5de732f0099ec2d
耗子 <i@haozi.net>
2024-11-07 00:52:12 +0800

    feat: 优化数据库模型

--- internal/biz/database.go
+++ internal/biz/database.go
@@ -10,17 +10,25 @@ import (
 	"github.com/TheTNB/panel/internal/app"
 )
 
+type DatabaseType string
+
+const (
+	DatabaseTypeMysql      DatabaseType = "mysql"
+	DatabaseTypePostgresql DatabaseType = "postgresql"
+	DatabaseTypeRedis      DatabaseType = "redis"
+)
+
 type Database struct {
-	ID        uint      `gorm:"primaryKey" json:"id"`
-	Name      string    `gorm:"not null;unique" json:"name"`
-	Type      string    `gorm:"not null" json:"type"`
-	Host      string    `gorm:"not null" json:"host"`
-	Port      int       `gorm:"not null" json:"port"`
-	Username  string    `gorm:"not null" json:"username"`
-	Password  string    `gorm:"not null" json:"password"`
-	Remark    string    `gorm:"not null" json:"remark"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID        uint         `gorm:"primaryKey" json:"id"`
+	Name      string       `gorm:"not null;unique" json:"name"`
+	Type      DatabaseType `gorm:"not null" json:"type"`
+	Host      string       `gorm:"not null" json:"host"`
+	Port      int          `gorm:"not null" json:"port"`
+	Username  string       `gorm:"not null" json:"username"`
+	Password  string       `gorm:"not null" json:"password"`
+	Remark    string       `gorm:"not null" json:"remark"`
+	CreatedAt time.Time    `json:"created_at"`
+	UpdatedAt time.Time    `json:"updated_at"`
 
 	DatabaseItems []*DatabaseItem `json:"-"`
 }
