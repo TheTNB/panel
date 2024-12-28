f3baff2aeb109e28f6001e946f18993cda7fa851
耗子 <i@haozi.net>
2024-12-29 02:11:04 +0800

    fix: 版权声明

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2022 - now  HaoZi Technology Co., Ltd.
+Copyright (C) 2022 - now  Rat Technology Co., Ltd.
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published
